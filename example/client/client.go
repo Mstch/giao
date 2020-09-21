4fe2f26443104c40bf9f618b1066655e591810e4
Tiddar <1124206990@qq.com>
2020-09-21 16:57:01 +0800

    feat: add example

--- example/client/client.go
+++ example/client/client.go
@@ -3,32 +3,36 @@ package main
 import (
 	"github.com/Mstch/giao"
 	test "github.com/Mstch/giao/example/msg"
-	"github.com/Mstch/giao/internal/server"
+	"github.com/Mstch/giao/internal/client"
 	"github.com/gogo/protobuf/proto"
 	"sync"
 )
 
 const TestRpc = 0
 
-func Test(req proto.Message, writer giao.ProtoWriter) {
-	respMsg := &test.Echo{}
-	respMsg.Content = req.(*test.Echo).Content
-	err := writer(TestRpc, respMsg)
-	if err != nil {
-		panic(err)
-	}
+func TestRespHandler(in proto.Message, out giao.ProtoWriter) {
+	println(string(in.(*test.Echo).Content))
 }
 
 func main() {
 	testMsgPool := &sync.Pool{New: func() interface{} {
 		return &test.Echo{}
 	}}
-	shandler := &giao.Handler{
-		H:       Test,
+	chandler := &giao.Handler{
+		H:       TestRespHandler,
 		ReqPool: testMsgPool,
 	}
-	s := server.NewStupidServer().RegWithId(TestRpc, shandler)
-	err := s.Listen("tcp", ":8888")
+	c, err := client.NewStupidClient().RegWithId(TestRpc, chandler).Connect("tcp", ":8888")
+	if err != nil {
+		panic(err)
+	}
+	msg := testMsgPool.Get().(*test.Echo)
+	msg.Content = []byte("fuck")
+	err = c.Go(TestRpc, msg)
+	if err != nil {
+		panic(err)
+	}
+	err = c.Serve()
 	if err != nil {
 		panic(err)
 	}
