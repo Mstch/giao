2f64ee548804c58dbd54bcc356bbc9aee6990e6f
Tiddar <1124206990@qq.com>
2020-09-21 16:57:01 +0800

    feat: add example

--- example/server/server.go
+++ example/server/server.go
@@ -1,1 +1,35 @@
-package example
+package main
+
+import (
+	"github.com/Mstch/giao"
+	test "github.com/Mstch/giao/example/msg"
+	"github.com/Mstch/giao/internal/server"
+	"github.com/gogo/protobuf/proto"
+	"sync"
+)
+
+const TestRpc = 0
+
+func TestRespHandler(in proto.Message, out giao.ProtoWriter) {
+	respMsg := &test.Echo{}
+	respMsg.Content = in.(*test.Echo).Content
+	err := out(TestRpc, respMsg)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	testMsgPool := &sync.Pool{New: func() interface{} {
+		return &test.Echo{}
+	}}
+	shandler := &giao.Handler{
+		H:       TestRespHandler,
+		ReqPool: testMsgPool,
+	}
+	s := server.NewStupidServer().RegWithId(TestRpc, shandler)
+	err := s.Listen("tcp", ":8888")
+	if err != nil {
+		panic(err)
+	}
+}
