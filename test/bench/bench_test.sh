26f9d52d3cf5890e879273b5b5a74f6ed734576b
Tiddar <1124206990@qq.com>
2020-09-22 10:18:26 +0800

    test: add bench test Description

--- test/bench/bench_test.sh
+++ test/bench/bench_test.sh
@@ -1,20 +1,3 @@
 go test -run=InitServer &
 sleep 3s
-go test -run=None -benchmem -count 5 -bench . -timeout 1h | tee benchresult && benchstat -sort delta benchresult
-#name          time/op
-#Stp1C-8       1.30µs ±130%
-#Stp16C-8       7.34µs ± 9%
-#SyncStd1C-8    20.8µs ±11%
-#SyncStd16C-8   14.5µs ± 7%
-#
-#name          alloc/op
-#Stp1C-8         1.00B ± 0%
-#Stp16C-8         742B ±20%
-#SyncStd1C-8      842B ± 9%
-#SyncStd16C-8     790B ± 0%
-#
-#name          allocs/op
-#Stp1C-8          0.00
-#Stp16C-8         2.00 ± 0%
-#SyncStd1C-8      12.0 ± 0%
-#SyncStd16C-8     12.0 ± 0%
+go test -run=None -benchmem -count 5 -bench . -timeout 1h | tee benchresult && benchstat -sort delta benchresult >benchstatresult
