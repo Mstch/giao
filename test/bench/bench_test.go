44ce9eecc40746444760aa304d0458e0806f5f3f
Tiddar <1124206990@qq.com>
2020-09-23 16:51:42 +0800

    test: grow packet to 64~64k (avg 5.4k)

--- test/bench/bench_test.go
+++ test/bench/bench_test.go
@@ -19,7 +19,7 @@ var EchoMsg = make([]*test.Echo, 12)
 func init() {
 	for i := range EchoMsg {
 		EchoMsg[i] = &test.Echo{}
-		ContentLen := 1
+		ContentLen := 16
 		for j := 0; j < i; j++ {
 			ContentLen <<= 1
 		}
@@ -31,12 +31,13 @@ func init() {
 	}
 }
 
+
 var echoPool = &sync.Pool{New: func() interface{} {
 	return &test.Echo{}
 }}
 
 func BenchmarkStp1C(b *testing.B) {
-	b.SetBytes(343 * 2)
+	b.SetBytes(5462 * 2)
 	w := sync.WaitGroup{}
 	chandler := &giao.Handler{
 		H: func(req proto.Message, respWriter giao.ProtoWriter) {
@@ -67,7 +68,7 @@ func BenchmarkStp1C(b *testing.B) {
 	w.Wait()
 }
 func BenchmarkStp16C(b *testing.B) {
-	b.SetBytes(343 * 2)
+	b.SetBytes(5462 * 2)
 	w := sync.WaitGroup{}
 	chandler := &giao.Handler{
 		H: func(req proto.Message, respWriter giao.ProtoWriter) {
@@ -124,7 +125,7 @@ func BenchmarkStp16C(b *testing.B) {
 	w.Wait()
 }
 func BenchmarkSyncStd1C(b *testing.B) {
-	b.SetBytes(343 * 2)
+	b.SetBytes(5462 * 2)
 	c, err := rpc.Dial("tcp", "localhost:8080")
 	w := sync.WaitGroup{}
 	w.Add(b.N)
@@ -144,7 +145,7 @@ func BenchmarkSyncStd1C(b *testing.B) {
 	w.Wait()
 }
 func BenchmarkSyncStd16C(b *testing.B) {
-	b.SetBytes(343 * 2)
+	b.SetBytes(5462 * 2)
 	w := sync.WaitGroup{}
 	w.Add(b.N)
 	for i := 0; i < 16; i++ {
@@ -186,7 +187,7 @@ func BenchmarkSyncStd16C(b *testing.B) {
 }
 
 func BenchmarkGrpc1C(b *testing.B) {
-	b.SetBytes(343 * 2)
+	b.SetBytes(5462 * 2)
 	conn, err := grpc.Dial("localhost:8181", grpc.WithInsecure())
 	c := test.NewEchoServiceClient(conn)
 	if err != nil {
@@ -207,7 +208,7 @@ func BenchmarkGrpc1C(b *testing.B) {
 func BenchmarkGrpc16C(b *testing.B) {
 	w := sync.WaitGroup{}
 	w.Add(b.N)
-	b.SetBytes(343 * 2)
+	b.SetBytes(5462 * 2)
 	for i := 0; i < 16; i++ {
 		conn, err := grpc.Dial("localhost:8181", grpc.WithInsecure())
 		c := test.NewEchoServiceClient(conn)
@@ -247,81 +248,3 @@ func BenchmarkGrpc16C(b *testing.B) {
 	w.Wait()
 }
 
-//func BenchmarkAStd1C(b *testing.B) {
-//	done := make(chan *rpc.Call, 1024)
-//	c, err := rpc.Dial("tcp", "localhost:8080")
-//	if err != nil {
-//		panic(err)
-//	}
-//	go func() {
-//		for j := 0; j < b.N; j++ {
-//			call := c.Go("StandardEcho.DoEcho", EchoMsg[j%12], &test.StandardEcho{}, done)
-//			if call.Error != nil {
-//				panic(call.Error)
-//			}
-//		}
-//	}()
-//	b.ResetTimer()
-//	for i := 0; i < b.N; i++ {
-//		<-done
-//	}
-//}
-//func BenchmarkAStd16C(b *testing.B) {
-//	done := make([]chan *rpc.Call, 16)
-//	for i := 0; i < 16; i++ {
-//		done[i] = make(chan *rpc.Call, 1024)
-//	}
-//	for i := 0; i < 16; i++ {
-//		c, err := rpc.Dial("tcp", "localhost:8080")
-//		if err != nil {
-//			panic(err)
-//		}
-//		go func(index int) {
-//			for j := 0; j < b.N/16; j++ {
-//				msg := &test.StandardEcho{}
-//				msg.Content = EchoMsg[j%12].Content
-//				msg.Index = int32(index)
-//				c.Go("StandardEcho.DoEcho", msg, &test.StandardEcho{}, done[index])
-//
-//			}
-//		}(i)
-//	}
-//	c, err := rpc.Dial("tcp", "localhost:8080")
-//	if err != nil {
-//		panic(err)
-//	}
-//	go func(index int) {
-//		for j := 0; j < b.N%16; j++ {
-//			msg := &test.StandardEcho{}
-//			msg.Content = EchoMsg[j%12].Content
-//			msg.Index = int32(index)
-//			c.Go("StandardEcho.DoEcho", msg, &test.StandardEcho{}, done[index])
-//
-//		}
-//	}(0)
-//	b.ResetTimer()
-//	for i := 0; i < b.N; i++ {
-//		var call *rpc.Call
-//		select {
-//		case call = <-done[0]:
-//		case call = <-done[1]:
-//		case call = <-done[2]:
-//		case call = <-done[3]:
-//		case call = <-done[4]:
-//		case call = <-done[5]:
-//		case call = <-done[6]:
-//		case call = <-done[7]:
-//		case call = <-done[8]:
-//		case call = <-done[9]:
-//		case call = <-done[0]:
-//		case call = <-done[11]:
-//		case call = <-done[12]:
-//		case call = <-done[13]:
-//		case call = <-done[14]:
-//		case call = <-done[15]:
-//		}
-//		if call.Error != nil {
-//			panic(call.Error)
-//		}
-//	}
-//}
